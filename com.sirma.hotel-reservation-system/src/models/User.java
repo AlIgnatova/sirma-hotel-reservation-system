7b6f1c9bf7f56f3309433971ea4621b36384f605
alexa <alexabdra.ignatova@gmail.com>
2024-11-30 19:48:50 +0200

    User class was modified

--- com.sirma.hotel-reservation-system/src/models/User.java
+++ com.sirma.hotel-reservation-system/src/models/User.java
@@ -1,8 +1,38 @@
 package models;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.List;
 
 public class User {
+    private String username;
+    private String password;
+    private List<String> bookingHistory;
 
+    public User(String username, String password, List<String> bookingHistory) {
+        this.username = username;
+        this.password = password;
+        this.bookingHistory = bookingHistory;
+    }
+
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    private String getPassword() {
+        return this.password;
+    }
+
+    public List<String> getBookingHistory() {
+        return this.bookingHistory;
+    }
+
+    public void addBookingToHistory(String bookingDetails) {
+        this.bookingHistory.add(bookingDetails);
+    }
+
+    public void printUserProfile() {
+        System.out.println("Username: " + this.username);
+        System.out.println("Booking History: " + this.bookingHistory);
+    }
 }
