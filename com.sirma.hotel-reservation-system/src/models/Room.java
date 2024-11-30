366818fa7d87bb01a22750642a89a5ed3e6f8731
alexa <alexabdra.ignatova@gmail.com>
2024-11-30 19:46:19 +0200

    Room class is modified

--- com.sirma.hotel-reservation-system/src/models/Room.java
+++ com.sirma.hotel-reservation-system/src/models/Room.java
@@ -1,5 +1,50 @@
 package models;
 
 public class Room {
+    private String roomNumber;
+    private RoomType roomType;
+    private double pricePerNight;
+    private double cancellationFee;
+    private String status;
 
+    public Room(String roomNumber, RoomType roomType, double pricePerNight, double cancellationFee, String status) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.pricePerNight = pricePerNight;
+        this.cancellationFee = cancellationFee;
+        this.status = status;
+    }
+
+
+    public String getRoomNumber() {
+        return this.roomNumber;
+    }
+
+    public RoomType getRoomType() {
+        return this.roomType;
+    }
+
+    public double getPricePerNight() {
+        return this.pricePerNight;
+    }
+
+    public double getCancellationFee() {
+        return this.cancellationFee;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void printRoomDetails() {
+        System.out.println("Room Number: " + this.roomNumber);
+        System.out.println("Type: " + this.roomType.getTypeName());
+        System.out.println("Price per Night: $" + this.pricePerNight);
+        System.out.println("Cancellation Fee: $" + this.cancellationFee);
+        System.out.println("Status: " + this.status);
+    }
 }
