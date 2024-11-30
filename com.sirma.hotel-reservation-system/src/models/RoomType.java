86f7dae994717f364999268aa40a2bcda34274a4
alexa <alexabdra.ignatova@gmail.com>
2024-11-30 19:43:14 +0200

    RoomType class is modified

--- com.sirma.hotel-reservation-system/src/models/RoomType.java
+++ com.sirma.hotel-reservation-system/src/models/RoomType.java
@@ -4,4 +4,32 @@ import java.util.List;
 
 public class RoomType {
 
+    private String typeName;
+    private int maxOccupancy;
+    private List<String> amenities;
+
+    public RoomType(String typeName, int maxOccupancy, List<String> amenities) {
+        this.typeName = typeName;
+        this.maxOccupancy = maxOccupancy;
+        this.amenities = amenities;
+    }
+
+    public String getTypeName() {
+        return this.typeName;
+    }
+
+    public int getMaxOccupancy() {
+        return this.maxOccupancy;
+    }
+
+    public List<String> getAmenities() {
+        return this.amenities;
+    }
+
+    public void printRoomTypeDetails() {
+        System.out.println("Room Type: " + this.typeName);
+        System.out.println("Max Occupancy: " + this.maxOccupancy);
+        System.out.println("Amenities: " + this.amenities);
+    }
+
 }
